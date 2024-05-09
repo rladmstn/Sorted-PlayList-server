54f7f4064cb5bd0db5f951a015e15557a5c7e4d7
rladmstn <oz33177351@gmail.com>
2024-05-09 22:01:06 +0900

    initial commit for project

---
+++ src/main/java/com/my/sorted_playlist/SortedPlaylistApplication.java
@@ -0,0 +1,13 @@
+package com.my.sorted_playlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortedPlaylistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SortedPlaylistApplication.class, args);
+	}
+
+}
