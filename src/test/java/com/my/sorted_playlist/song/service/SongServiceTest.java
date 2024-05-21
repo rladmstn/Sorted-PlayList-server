ed88c10a9957fe5305ad338c9cce7e8d889f49d2
rladmstn <oz33177351@gmail.com>
2024-05-21 14:39:30 +0900

    test : 플레이리스트의 노래 목록 조회 테스트 작성
    플레이리스트의 노래 목록 조회 성공, 존재하지 않는 플레이리스트로 조회 실패, 주인 불일치로 인한 실패 테스트 작성

--- src/test/java/com/my/sorted_playlist/song/service/SongServiceTest.java
+++ src/test/java/com/my/sorted_playlist/song/service/SongServiceTest.java
@@ -5,6 +5,8 @@ import static org.mockito.Mockito.*;
 
 import java.lang.reflect.Field;
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -24,6 +26,7 @@ import com.my.sorted_playlist.playlist.exception.PlaylistPermissionException;
 import com.my.sorted_playlist.playlist.repository.PlaylistRepository;
 import com.my.sorted_playlist.song.domain.Song;
 import com.my.sorted_playlist.song.dto.AddSongRequest;
+import com.my.sorted_playlist.song.dto.GetSongResponse;
 import com.my.sorted_playlist.song.repository.SongRepository;
 import com.my.sorted_playlist.user.domain.User;
 
@@ -41,10 +44,15 @@ class SongServiceTest {
 	private User user;
 	private User diffUser;
 	private Playlist playlist;
+	private Song song1;
+	private Song song2;
 	private final String songVideoId = "SONGVIDEOID";
 	private final String title = "title";
 	private final String singer = "singer";
 	private final String songLength = "3:03";
+	private final Long playlistId = 10L;
+	private final Long songId1 = 30L;
+	private final Long songId2 = 31L;
 
 	@BeforeEach
 	void set() throws IllegalAccessException, NoSuchFieldException {
@@ -53,17 +61,26 @@ class SongServiceTest {
 
 		playlist = Playlist.builder().name("playlist").user(user).createdDateTime(LocalDateTime.now()).songCount(0).build();
 
+		song1 = Song.builder().playlist(playlist).title("title1").singer("singer1")
+			.songLength("songLength1").songPlayCount(0).songVideoId("songVideoId1")
+			.addedDateTime(LocalDateTime.now()).lastPlayedDateTime(LocalDateTime.now()).build();
+		song2 = Song.builder().playlist(playlist).title("title2").singer("singer2")
+			.songLength("songLength2").songPlayCount(0).songVideoId("songVideoId2")
+			.addedDateTime(LocalDateTime.now()).lastPlayedDateTime(LocalDateTime.now()).build();
+
 		Field userIdField = User.class.getDeclaredField("id");
 		userIdField.setAccessible(true);
 		userIdField.set(user, 1L);
-
-		Field userIdField2 = User.class.getDeclaredField("id");
-		userIdField2.setAccessible(true);
-		userIdField2.set(diffUser,2L);
+		userIdField.set(diffUser,2L);
 
 		Field playlistIdField = Playlist.class.getDeclaredField("id");
 		playlistIdField.setAccessible(true);
-		playlistIdField.set(playlist, 10L);
+		playlistIdField.set(playlist, playlistId);
+
+		Field songIdField = Song.class.getDeclaredField("id");
+		songIdField.setAccessible(true);
+		songIdField.set(song1,songId1);
+		songIdField.set(song2,songId2);
 	}
 
 	@Test
@@ -109,4 +126,23 @@ class SongServiceTest {
 			.hasFieldOrPropertyWithValue("status", HttpStatus.FORBIDDEN.value())
 			.hasFieldOrPropertyWithValue("error","플레이리스트의 주인과 사용자가 일치하지 않습니다.");
 	}
+
+	@Test
+	@DisplayName("플레이리스트의 노래 목록 조회 성공")
+	void 플레이리스트노래목록조회_성공(){
+		// given
+		List<Song> songs = Arrays.asList(song1, song2);
+		when(playlistRepository.findById(playlistId)).thenReturn(Optional.ofNullable(playlist));
+		when(songRepository.findAllByPlaylist(playlist)).thenReturn(songs);
+		// when
+		List<GetSongResponse> result = songService.getSongsInPlaylist(user, playlistId);
+		// then
+		verify(songRepository,times(1)).findAllByPlaylist(playlist);
+		assertThat(result).hasSize(2);
+		assertThat(result.get(0).title()).isEqualTo("title1");
+		assertThat(result.get(1).title()).isEqualTo("title2");
+		assertThat(result.get(0).id()).isEqualTo(songId1);
+		assertThat(result.get(1).id()).isEqualTo(songId2);
+	}
+
 }
